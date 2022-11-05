e2d00107a8c89424eadcd1a3c630150c024b2a0b
Savitskiy <sovick90@bk.ru>
2022-11-05 20:13:19 +0300

    _id

--- middlewares/validation.js
+++ middlewares/validation.js
@@ -48,7 +48,7 @@ const movieValidate = celebrate({
 
 const movieIdValidate = celebrate({
   params: Joi.object().keys({
-    cardId: Joi.string().hex().length(24),
+    _id: Joi.string().hex().length(24),
   }),
 });
 
