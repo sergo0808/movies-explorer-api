623d1826e1a3bc94674292ba64d5ed210bc060d2
Savitskiy <sovick90@bk.ru>
2022-11-07 17:49:57 +0300

    validation email

--- middlewares/validation.js
+++ middlewares/validation.js
@@ -21,7 +21,7 @@ const registerValidate = celebrate({
 const userValidate = celebrate({
   body: Joi.object().keys({
     name: Joi.string().required().min(2).max(30),
-    email: Joi.string().required(),
+    email: Joi.string().required().email(),
   }),
 });
 
